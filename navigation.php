1e68fc8764874c16ea5bf4712dd0637793c91f36
Swati Gautam <swati.mcs18.du@gmail.com>
2020-06-25 19:31:51 +0530

    remove unused navigations

--- navigation.php
+++ navigation.php
@@ -2,16 +2,6 @@
 
 return [
     'Getting Started' => 'docs/getting-started',
-    'Controllers' => [
-        'url' => 'docs/controllers',
-        'children' => [
-            'Account Controller' => 'docs/controllers/account-controller',
-            'Attachments Controller' => 'docs/controllers/attachments-controller',
-            'College Controller' => 'docs/controllers/college-controller',
-        ],
-    ],
-    'Models' => 'docs/models',
-    'Custom Validation Rules' => 'docs/rules',
     'Permissions' => 'docs/permissions',
     'Authorization Policies' => 'docs/policies',
     'Problem Statment' => 'docs/problem-statment',
